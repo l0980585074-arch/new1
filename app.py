712c9d3d00dbe23ed4ce19cd10ebdca56333c9d1
l0980585074-arch <l0980585074@gmail.com>
2025-11-08 20:58:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,7 @@ def signal():
     return {"status": "ok"}
 
 if pionex-webhook == "main":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
